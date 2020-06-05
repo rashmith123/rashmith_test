7bbc808f509e5e025d3b155547f95a62d9de9a26
Rashmith <mdrashmith.navodana@gmail.com>
2020-06-05 18:30:57 +0530

    SignIn component UI

--- App.js
+++ App.js
@@ -1,19 +1,77 @@
 import React  from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>SignIn</Text>
+<View >   
+  <View style={ styles.container}>
+    <View style={ styles.childcontainer}>
+      <View style={ styles.childchildcontainer}>
+        <Text style={ styles.Text}>E mail</Text>
+      </View>
+     <TextInput style={ styles.input}/>
+
+   </View>
+   <View style={ styles.childcontainer}>
+     <View style={ styles.childchildcontainer}>
+        <Text style={ styles.Text}>Password</Text>
+     </View>
+        <TextInput secureTextEntry={true} style={ styles.input}/>
     </View>
+    <View style={ {height:50, width:80,margin:10, alignSelf:'flex-end'}}>
+        <Button title="Sign-in"  ></Button>
+    </View>
+  </View>
+
+</View> 
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column',
+    backgroundColor:"lightgray",
+    marginVertical: 50,
+    marginHorizontal: 20,
+    borderColor: '#000',
+    borderRadius: 5,
+    borderWidth: 5,
+    borderStyle: 'solid'
+
   },
+  childcontainer: {
+    flexDirection:'row',
+
+    width:'95%',
+     margin:5,
+     alignSelf:'center',
+     
+  },
+  childchildcontainer: {
+    width:'25%',
+     margin:5,
+     alignSelf:'flex-start',
+    
+
+  },
+  input:{
+    fontSize:16,
+    alignSelf: 'flex-end',
+    backgroundColor:'white',
+    flex:1,
+
+     
+  },
+  
+  Text:{
+    flex:1,
+    fontSize:16,
+    marginLeft:10,
+    marginTop: 10,
+    marginBottom: 10,
+    alignItems: 'center',
+    alignSelf:'flex-start',    
+  }, 
+
 });
+
