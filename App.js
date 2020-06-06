7b983370a2a147acb88ce1b971c6e6ca8b2e92dd
Rashmith <mdrashmith.navodana@gmail.com>
2020-06-06 21:29:04 +0530

    SignIn component validations

--- App.js
+++ App.js
@@ -20,7 +20,6 @@ export default class App extends Component {
      
     var reg = /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
      if (reg.test(this.state.username) === true){
-        alert('Valid email');
         this.setState({email_statement: 'Valid Email'});
     }
     else{
